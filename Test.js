2607a59766619c8a0aff87e49147f622a78142b6
Kaushik Roy Chowdhury <devedkaushik@gmail.com>
2024-05-30 21:36:31 +1200

    Initial

---
+++ Test.js
@@ -0,0 +1,10 @@
+// Testing IntelliSense
+function greet(name) {
+    console.log(`Hello, ${name}`);
+}
+
+greet("World!");
+
+// Using document object
+//document.getElementById('myElement').innerText = 'Hello, IntelliSense!';
+document.getElementById('krc').innerHTML
